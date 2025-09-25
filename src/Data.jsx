600b9f3dea4abf2ba0da8fa4586015c732aaeb4f
besttimilehin122-afk <besttimilehin122@gmail.com>
2025-09-25 16:39:02 +0100

    Update Data.jsx

--- src/Data.jsx
+++ src/Data.jsx
@@ -4,7 +4,7 @@ function Data() {
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    fetch("http://34.245.32.196:5000/data")
+    fetch("http://54.195.219.233:5000//data")
       .then((res) => res.json())
       .then((json) => {
         console.log(json);
